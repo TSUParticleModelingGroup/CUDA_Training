6c6aea2936123e0dc8d167b0c401c388c9817593
Bryant Wyatt <wyatt@tarleton.edu>
2021-01-18 16:50:43 -0600

    Add files via upload

---
+++ Vector Addition/VectorAddCPU/VectorAdditionCPU.cu
@@ -0,0 +1,84 @@
+// To compile: nvcc VectorAdditionCPU.cu -O3 -o temp1 -lcudart
+// To run: ./temp
+#include <sys/time.h>
+#include <stdio.h>
+
+#define N 67107840 
+
+float *A_CPU, *B_CPU, *C_CPU; //CPU pointers
+
+void AllocateMemory()
+{					
+	//Allocate Host (CPU) Memory
+	A_CPU = (float*)malloc(N*sizeof(float));
+	B_CPU = (float*)malloc(N*sizeof(float));
+	C_CPU = (float*)malloc(N*sizeof(float));
+}
+
+//Loads values into vectors that we will add.
+void Innitialize()
+{
+	int i;
+	
+	for(i = 0; i < N; i++)
+	{		
+		A_CPU[i] = (float)i;	
+		B_CPU[i] = (float)i;
+	}
+}
+
+//Cleaning up memory after we are finished.
+void CleanUp(float *A_CPU,float *B_CPU,float *C_CPU)
+{
+	free(A_CPU); free(B-O_CPU); free(C_CPU);
+}
+
+//Adds vectors A and B then stores result in vector C
+void Addition(float *A, float *B, float *C, int n)
+{
+	int id;
+	for(id = 0; id < n; id++)
+	{ 
+		C[id] = A[id] + B[id];
+	}
+}
+
+int main()
+{
+	int i;
+	timeval start, end;
+	
+	//Partitioning off the memory that you will be using.	
+	AllocateMemory();
+
+	//Loading up values to be added.
+	Innitialize();
+
+	//Starting the timer	
+	gettimeofday(&start, NULL);
+
+	//Add the two vectors
+	Addition(A_CPU, B_CPU ,C_CPU, N);
+
+	//Stopping the timer
+	gettimeofday(&end, NULL);
+	
+	//Calculating the total time used in the addition and converting it to milliseconds
+	float time = (end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec);
+
+	// Displaying vector info you will want to comment out the vector print line when your
+	//vector becomes big. This is just to make sure everything is running correctly.
+	printf("CPU Time in milliseconds= %.15f\n", (time/1000.0));	
+	for(i = 0; i < N; i++)		
+	{		
+		//printf("A[%d] = %.15f  B[%d] = %.15f  C[%d] = %.15f\n", i, A_CPU[i], i, B_CPU[i], i, C_CPU[i]);
+	}
+
+	//Displaying the last value of the addition for a check when all vector display has been commented out.
+	printf("Last Values are A[%d] = %.15f  B[%d] = %.15f  C[%d] = %.15f\n", N-1, A_CPU[N-1], N-1, B_CPU[N-1], N-1, C_CPU[N-1]);
+
+	//Your done so cleanup your mess.	
+	CleanUp(A_CPU,B_CPU,C_CPU);	
+	
+	return(0);
+}
